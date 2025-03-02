462b9d42fd9f14760b6cac87641f3ea772aabd6c
tlamngu <tlamngu@gmail.com>
2025-03-02 21:41:36 +0700

    Test

--- routes/storyRoutes.js
+++ routes/storyRoutes.js
@@ -1,4 +1,3 @@
-// routes/storyRoutes.js
 import express from 'express';
 import {
     createStory,
@@ -14,13 +13,13 @@ const router = express.Router();
 // --- Create Story ---
 router.post('/', verifyToken, createStory); // POST /api/v2/stories
 
-// --- Get All Stories (Paginated, Sorted) ---
+// --- Get All Stories (limited, Sorted) ---
 router.get('/', getAllStories); // GET /api/v2/stories
 
 // --- Get Story by ID ---
 router.get('/:id', getStory); // GET /api/v2/stories/:id
 
-// --- Get Stories by Creator (Paginated) ---
+// --- Get Stories by Creator (limit) ---
 router.get('/creator/:creatorId', getStoriesByCreator); // GET /api/v2/stories/creator/:creatorId
 
 // --- Delete Story ---
